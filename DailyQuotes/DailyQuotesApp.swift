3c9e4641ad06527cf3eacde429f01aad3ccabb07
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-04-10 00:38:32 +0400

    Initial Commit

---
+++ DailyQuotes/DailyQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuotesApp.swift
+//  DailyQuotes
+//
+//  Created by aplle on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
